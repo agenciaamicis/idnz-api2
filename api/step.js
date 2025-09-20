07e1e2a3654cf92644816eec9aa4b98990e588be
agenciaamicis <agenciaamicis@bol.com.br>
2025-09-20 01:10:07 -0300

    Update step.js

--- api/step.js
+++ api/step.js
@@ -80,7 +80,7 @@ export default function handler(req, res) {
       userData: {
         verified: true
       },
-      redirectUrl: "https://atendimentoconsulta.services/7649005498/iddz/"
+      redirectUrl: "https://superconsultas.info/vlr-g/"
     }
   };
 
@@ -95,6 +95,7 @@ export default function handler(req, res) {
   // Retornar o step
   res.status(200).json(steps[stepId]);
 }
+
 
 
 
